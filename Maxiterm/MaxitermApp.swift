3d378198cc7748ceb09fc8645fe90c957b3e4d89
anthony BAUCAL <support@digistream.fr>
2026-06-17 11:43:47 +0200

    Initial Commit

---
+++ Maxiterm/MaxitermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxitermApp.swift
+//  Maxiterm
+//
+//  Created by Anthony BAUCAL on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaxitermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaxitermTests/MaxitermTests.swift
@@ -0,0 +1,18 @@
+//
+//  MaxitermTests.swift
+//  MaxitermTests
+//
+//  Created by Anthony BAUCAL on 17/06/2026.
+//
+
+import Testing
+
+struct MaxitermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
